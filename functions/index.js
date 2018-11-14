786d48117e362a7448ffe9846e69130325d675d0
bbowman <bbowman@spillman.com>
2018-11-14 09:17:11 -0700

    Add welcome message cloud func

--- functions/index.js
+++ functions/index.js
@@ -14,9 +14,27 @@
  * limitations under the License.
  */
 
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+// Import the Firebase SDK for Google Cloud Functions.
+const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// TODO(DEVELOPER): Write the addWelcomeMessages Function here.
+
+// Adds a message that welcomes new users into the chat.
+exports.addWelcomeMessages = functions.auth.user().onCreate(async (user) => {
+  console.log('A new user signed in for the first time.');
+  const fullName = user.displayName || 'Anonymous';
+
+  // Saves the new welcome message into the database
+  // which then displays it in the FriendlyChat clients.
+  await admin.database().ref('messages').push({
+    name: 'Firebase Bot',
+    profilePicUrl: '../public/images/firebase-logo.png', // Firebase logo
+    text: `${fullName} signed in for the first time! Welcome!`,
+  });
+  console.log('Welcome message written to database.');
+});
 
 // TODO(DEVELOPER): Write the blurOffensiveImages Function here.
 
